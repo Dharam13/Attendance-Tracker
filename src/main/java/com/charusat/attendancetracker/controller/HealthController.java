23503b36a8d7eaae3c365fa66185369861e3fe85
Patel Dharam Hiteshbhai <dharampatel1309@gmail.com>
2025-04-25 16:55:25 +0530

    Update HealthController.java

--- src/main/java/com/charusat/attendancetracker/controller/HealthController.java
+++ src/main/java/com/charusat/attendancetracker/controller/HealthController.java
@@ -1,18 +1,13 @@
 package com.charusat.attendancetracker.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/")
     public String home() {
-        return "Application is running!";
+        return "Charusat Attendance Tracker is working!";
     }
 }
